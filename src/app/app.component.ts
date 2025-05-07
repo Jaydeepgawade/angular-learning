3bb541764b7caa594d3537390368b2a20a6e04b8
Jaydeep Gawade <jaydeepgawade42@gmail.com>
2025-05-07 08:28:31 +0530

    Added Student and Trainer components with UI update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { StudentInfoComponent } from './student-info/student-info.component';
 import { RouterOutlet } from '@angular/router';
+import { TrainerInfoComponent } from "./trainer-info/trainer-info.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StudentInfoComponent, TrainerInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/trainer-info/trainer-info.component.ts
@@ -0,0 +1,21 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-trainer-info',
+//   imports: [],
+//   templateUrl: './trainer-info.component.html',
+//   styleUrl: './trainer-info.component.css'
+// })
+// export class TrainerInfoComponent {
+
+// }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainer-info',
+  standalone: true, 
+  templateUrl: './trainer-info.component.html',
+  styleUrls: ['./trainer-info.component.css'] 
+})
+export class TrainerInfoComponent {}
